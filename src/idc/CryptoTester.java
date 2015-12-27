7743929a476497b455551d142a2d9c21d9d8c820
JKILZI <JKILZI@JKILZI01.corp.amdocs.com>
2015-12-28 00:16:55 +0200

    Revert "Implemented the config file (very basic)"
    This reverts commit 75d26e933feb7b888d075f3f5b98160cbf1fef27.

--- src/idc/CryptoTester.java
+++ src/idc/CryptoTester.java
@@ -2,16 +2,7 @@ package idc;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.RandomAccessFile;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.security.cert.Certificate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
@@ -19,20 +10,12 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
-import com.sun.xml.internal.org.jvnet.staxex.Base64EncoderStream;
-import com.sun.xml.internal.ws.util.ByteArrayBuffer;
-
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
-import java.security.AlgorithmParameters;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.Signature;
 
 public class CryptoTester {
 
@@ -71,15 +54,15 @@ public class CryptoTester {
 		final int AES_KEYLENGTH = 128;	// change this as desired for the security level you want
 		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
 		keyGen.init(AES_KEYLENGTH);
-		SecretKey secretKey = keyGen.generateKey();
-		
+		SecretKey secretKey = keyGen.generateKey(); // TODO: must encrypt later with RSA and appended to the configuration file.
+
 		/*
 		 * Step 2. Generate a random Initialization Vector (IV) 
 		 * 		a. Use SecureRandom to generate random bits
 		 * 		   The size of the IV matches the blocksize of the cipher (128 bits for AES)
 		 * 		b. Construct the appropriate IvParameterSpec object for the data to pass to Cipher's init() method
 		 */
-		byte[] ivByteBuffer = new byte[AES_KEYLENGTH / 8]; 
+		byte[] ivByteBuffer = new byte[AES_KEYLENGTH / 8];  // TODO: Save the IV bytes to the configuration file later 
 		SecureRandom prng = new SecureRandom();
 		prng.nextBytes(ivByteBuffer);
 		IvParameterSpec iv = new IvParameterSpec(ivByteBuffer);
@@ -88,60 +71,39 @@ public class CryptoTester {
 		 * Step 3. Create a Cipher by specifying the following parameters
 		 * 		a. Algorithm name - here it is AES 
 		 * 		b. Mode - here it is CBC mode 
-		 * 		c. Padding - No padding
+		 * 		c. Padding - e.g. PKCS7 or PKCS5
 		 */
 		Cipher aesCipherForEncryption = Cipher.getInstance("AES/CBC/NoPadding"); // Must specify the mode explicitly as most JCE providers default to ECB mode!!
+
+		/*
+		 * Step 4. Initialize the Cipher for Encryption
+		 */
 		aesCipherForEncryption.init(Cipher.ENCRYPT_MODE, secretKey, iv);
 
 		/*
-		 * Step 5. Encrypt and Calculate Digital Signature on the fly
+		 * Step 5. Read the input file and encrypt it
 		 */		
-		Signature signer = Signature.getInstance("SHA1withRSA");
-		PrivateKey privateKey = keyPair.getPrivate(); // TODO: keyPair might be null!!!
-		signer.initSign(privateKey);
-
 		FileInputStream fis = new FileInputStream(INPUT_FILE);
 		FileOutputStream fos = new FileOutputStream(OUTPUT_FILE);
 		CipherOutputStream cos = new CipherOutputStream(fos, aesCipherForEncryption);
 		byte[] buffer = new byte[8];
 		int numBytesRead = fis.read(buffer);
-		signer.update(buffer);
 		while (numBytesRead != -1) {
 			cos.write(buffer, 0, numBytesRead);
 			numBytesRead = fis.read(buffer);
-			signer.update(buffer);
 		}
-		byte[] digitalSignature = signer.sign();
 		cos.flush();
 				
+	    /*
+	     * TODO: Step 7. Digital Signature
+	     */
+		
 		/*
-		 * Step 7. Create the configuration file
+		 * TODO: Step 8. Create the configuration file
 		 *      a. Add the Digital Signature
 		 *      b. Add the Secret Key
 		 *      b. Add the IV (TODO: as Plaintext?) 
 		 */
-		Cipher rsaCipher = Cipher.getInstance("RSA");
-		rsaCipher.init(Cipher.ENCRYPT_MODE, privateKey);
-		byte[] secretKeyEncrypted = rsaCipher.doFinal(secretKey.getEncoded());
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("Digital Signature: " + new BASE64Encoder().encode(digitalSignature));
-		sb.append(System.lineSeparator());
-		sb.append("Init Vector: " + new BASE64Encoder().encode(iv.getIV()));
-		sb.append(System.lineSeparator());
-//		sb.append("Secret Key (Encrypted): " + new BASE64Encoder().encode(secretKeyEncrypted));
-		String plaintextSecretKey = new String(secretKey.getEncoded(), StandardCharsets.UTF_16);
-		sb.append("Secret Key (Encrypted): " + plaintextSecretKey);
-		sb.append(System.lineSeparator());
-		String text = sb.toString();
-		try {
-			Files.write(Paths.get("config.txt"), text.getBytes());
-		} catch (FileAlreadyExistsException e) {
-			Logger.getLogger(CryptoTester.class.getName()).log(Level.SEVERE, null, e);
-		}
-//		AlgorithmParameters algParams = aesCipherForEncryption.getParameters();
-//		byte[] algParamsBytes = algParams.getEncoded();				
-		
 		
 		
 
