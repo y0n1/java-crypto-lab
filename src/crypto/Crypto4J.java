5566a0809a13444f56f599be94074bbbb7f7ed93
y0n1 <y0n1@outlook.com>
2015-12-30 03:26:20 +0200

    we now know how to retreive the keypairs

--- src/crypto/Crypto4J.java
+++ src/crypto/Crypto4J.java
@@ -8,10 +8,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Paths;
+import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableEntryException;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -22,7 +28,6 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  *
@@ -31,110 +36,144 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class Crypto4J {
 
     private static final Logger LOGGER = Logger.getLogger(Crypto4J.class.getName());
-    private static final Map<String, File> defaultKeystoreFiles = new HashMap(2);
     private static final String KEYSTORES_DIR = "keystores";
     private static final String OUTPUT_DIR = "output";
     private static final String INPUT_DIR = "input";
-    
-    /**
-     * For the sake of simplicity, both keystores have the same password.
-     */
-    private static char[] keyStoresPassword;
+
     private static FileInputStream fileInputStream;
     private static FileOutputStream fileOutputStream;
-    
-    
+
     public static byte[] encrypt() {
-        throw new NotImplementedException();
-    } 
-    
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
     public static String decrypt() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     public static byte[] sign() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     public static boolean verify() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     private static byte[] readFile(File fileName, InputStream inputStream) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
     private static int writeFile(File fileName, OutputStream OutputStream) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private static byte[] readFileSecure(File fileName, InputStream inputStream) {
-        throw new NotImplementedException();
-    }
-    
-    private static int writeFileSecure(File fileName, OutputStream OutputStream) {
-        throw new NotImplementedException();
-    }
-    
-    private static KeyStore loadKeyStore(String name) throws IllegalArgumentException,
-            NoSuchAlgorithmException, FileNotFoundException, KeyStoreException,
-            IOException, CertificateException {
-        
-        if (!name.matches("(alice|bob)")) {
-            throw new IllegalArgumentException("Arguments allowed are: 'alice' or 'bob'");
-        }
-        
+
+    private static KeyStore loadKeyStore(File keyStoreFileName, char[] keyStorePassword) throws IllegalArgumentException,
+            NoSuchAlgorithmException, FileNotFoundException, KeyStoreException, IOException, CertificateException {
+
         KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-        File keyStoreFile = defaultKeystoreFiles.get(name);
-        fileInputStream = new FileInputStream(keyStoreFile);
-        keystore.load(fileInputStream, keyStoresPassword);
-        
+        fileInputStream = new FileInputStream(keyStoreFileName);
+        keystore.load(fileInputStream, keyStorePassword);
+        fileInputStream.close();
+
         return keystore;
     }
-        
+
+    private static KeyPair retrieveKeyPair(String alias, char[] keyPass, File keyStoreFile, char[] keyStorePassword) throws IllegalArgumentException,
+            NoSuchAlgorithmException, KeyStoreException, IOException, FileNotFoundException, CertificateException, UnrecoverableKeyException {
+
+        KeyStore keyStore = loadKeyStore(keyStoreFile, keyStorePassword);
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, keyPass);
+        PublicKey publicKey = keyStore.getCertificate(alias).getPublicKey();
+
+        return new KeyPair(publicKey, privateKey);
+    }
+
     public static void main(String[] args) {
-        // Setup the default keystores that we'll be using
-        defaultKeystoreFiles.put("alice", Paths.get(KEYSTORES_DIR, "alice.jks").toFile());
-        defaultKeystoreFiles.put("bob", Paths.get(KEYSTORES_DIR, "bob.jks").toFile());
-                
-        Options options = new Options();
-        
-        // Definition Stage
-        boolean hasArg = true;
-        options.addOption("p", "password", hasArg, "The password used to protect the integrity of the keystore contents");
-        options.addOption("h", "help", !hasArg, "This help message");
-        
-        // Automatically generate the Usage + Help message
-        HelpFormatter formatter = new HelpFormatter();        
-        
-        // Parsing Stage
+        /* Setup the very specific details for the entities inside the keystores that we'll be using.
+         * REMARK:
+         *     This is the only sensitive information that I won't force it to be deleted immediately after being used.
+         *     Should this was not just an exercise, I wouldn't never leave this data in memory.
+         */
+        final Map<String, Map> defaultEntities = new HashMap(2);
+        defaultEntities.put("alice", new HashMap<>());
+        defaultEntities.get("alice").put("keystore", Paths.get(KEYSTORES_DIR, "alice.jks").toFile());
+        defaultEntities.get("alice").put("passphrase", "123456".toCharArray());
+        defaultEntities.put("bob", new HashMap<>());
+        defaultEntities.get("bob").put("keystore", Paths.get(KEYSTORES_DIR, "bob.jks").toFile());
+        defaultEntities.get("bob").put("passphrase", "123456".toCharArray());
+
+        final String usageMessage = "Crypto4J -[h|d|p]";
         CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        Options options = new Options();
+
+        // Define cli arguments
+        boolean hasArg = true;
+        options.addOption("h", "help", !hasArg, "This help message");
+        options.addOption("d", "debug", !hasArg, "Shows stack traces when an error is found");
+        options.addOption("p", "password", hasArg, "The password used to protect the integrity of the keystore contents");
+
+        boolean hasDebug = false;
         try {
-            // Interrogation Stage
-            CommandLine cmd = parser.parse(options, args);           
-                        
-            // Display Help message and exit
+            CommandLine cmd = parser.parse(options, args);
+
             if (cmd.hasOption("help")) {
-                formatter.printHelp(Crypto4J.class.getName(), options);
+                formatter.printHelp(usageMessage, options);
                 System.exit(0);
-            }            
-            
+            }
+
+            hasDebug = (cmd.hasOption("debug"));
+
+            char[] keyStorePassword = null;
             if (cmd.hasOption("password")) {
-                final String KS_DEFAULT_PASSWD = "k3y5t0r3";
-                keyStoresPassword = cmd.getOptionValue("password", KS_DEFAULT_PASSWD).toCharArray();
-                KeyStore aliceKeyStore = loadKeyStore("alice");
-                KeyStore bobKeyStore = loadKeyStore("bob");
-            }         
+                keyStorePassword = cmd.getOptionValue("password").toCharArray();
+            }
+
+            /* Schedule unnecesssary instance references for garbage collection.
+             * The main reason is that these objects may contain a copy of the plain
+             * text password passed by in the cli arguments.
+             * 
+             * QUESTION FOR THE GRADER: Is this the best solution Java has to offer?               
+             */
+            formatter = null;
+            options = null;
+            parser = null;
+            cmd = null;
+            System.gc(); // This is CPU intensive... but I had no choice... did I? Well ask James Gosling :)~
+
+            // Retrieve the key pairs
+            File aliceKeyStoreFile = (File) defaultEntities.get("alice").get("keystore");
+            char[] alicePassPharse = (char[]) defaultEntities.get("alice").get("passphrase");
+            KeyPair aliceKeyPair = retrieveKeyPair("alice", alicePassPharse, aliceKeyStoreFile, keyStorePassword);
             
+            File bobKeyStoreFile = (File) defaultEntities.get("bob").get("keystore");
+            char[] bobPassPharse = (char[]) defaultEntities.get("bob").get("passphrase");
+            KeyPair bobKeyPair = retrieveKeyPair("bob", bobPassPharse, bobKeyStoreFile, keyStorePassword);
+            
+            // Zero the keystore password
+            Arrays.fill(keyStorePassword, '\u0000');
+            
+            /*
+            TODO: 
+                1. Read the plaintext
+                2. PRNG: IV and SecretKey (for AES)
+                3. encrypt plaintext with "AES/ECB/PKCS5Padding"
+                4. produce the Digital Signature of the plaintext (use "SHA256withRSA")
+                5. write to file (XML?)
+            */ 
+
         } catch (ParseException ex) {
             System.err.println(ex.getLocalizedMessage());
-            formatter.printHelp(Crypto4J.class.getName(), options);
+            formatter.printHelp(usageMessage, options);
             System.exit(1);
-        } catch (IllegalArgumentException | KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException ex) {
-            Logger.getLogger(Crypto4J.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException | KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException | UnrecoverableEntryException | UnsupportedOperationException ex) {
+            if (hasDebug) {
+                LOGGER.log(Level.SEVERE, ex.getLocalizedMessage(), ex);
+            } else {
+                LOGGER.log(Level.SEVERE, ex.getLocalizedMessage());
+            }
             System.exit(1);
-        }       
-        
+        }
     }
-    
+
 }
